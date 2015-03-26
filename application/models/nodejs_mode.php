2cf4336a1e6231c30b92de7c1046cd3aceb8edb7
unknown <2275025@qq.com>
2015-03-26 23:43:49 +0800

    update

--- application/models/nodejs_mode.php
+++ application/models/nodejs_mode.php
@@ -10,12 +10,18 @@ class Nodejs_mode extends CI_Model {
 
     function __construct()
     {
+        $this -> load -> database();
         parent::__construct();
     }
     function getNodejsAll(){
-        $this -> load -> database();
+
         $query = $this->db->get("test");
         //var_dump($query);
         return $query->result();
+    }
+    function insertArticle($d){
+        //var_dump($d);
+        $this->db->insert('blog',$d);
+        var_dump($d);
     }
 }
