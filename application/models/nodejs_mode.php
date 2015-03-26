2cd3adc41cbf245a0c719551641891105f4c9dd9
ralcen <2275025@qq.com>
2015-03-26 18:07:48 +0800

    update

---
+++ application/models/nodejs_mode.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: XIAODI001
+ * Date: 2015/3/26
+ * Time: 17:11
+ */
+
+class Nodejs_mode extends CI_Model {
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+    function getNodejsAll(){
+        $this -> load -> database();
+        $query = $this->db->get("test");
+        //var_dump($query);
+        return $query->result();
+    }
+}
